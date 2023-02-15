f5fc6df25a57c3c2bc716bb4e4080d5448f8216a
Caio Fuccio <caio.fuccio@gmail.com>
2023-02-15 02:34:15 -0300

    feat: adding eslint configs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/hello', () => {
-    return 'Hello World';
-});
+  return 'Hello World'
+})
 
-app.listen({
+app
+  .listen({
     port: 8080,
-}).then(() => console.log('HTTP Server running on port 8080'));
+  })
+  .then(() => console.log('HTTP Server running on port 8080'))
